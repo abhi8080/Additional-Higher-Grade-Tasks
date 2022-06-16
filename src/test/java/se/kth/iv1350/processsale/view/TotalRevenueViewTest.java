6c04f2ec9522cc8170c10d87a6542c3d03ab7d9e
abhi8080 <99359469+abhi8080@users.noreply.github.com>
2022-06-16 10:19:53 +0200

    Update TotalRevenueViewTest.java

--- src/test/java/se/kth/iv1350/processsale/view/TotalRevenueViewTest.java
+++ src/test/java/se/kth/iv1350/processsale/view/TotalRevenueViewTest.java
@@ -6,27 +6,38 @@ import se.kth.iv1350.processsale.model.Amount;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 
 public class TotalRevenueViewTest {
-   
+
     private TotalRevenueView instance;
     private ByteArrayOutputStream printoutBuffer;
     private PrintStream originalSysOut;
 
+    @BeforeEach
+    public void setUpStreams() {
+        originalSysOut = System.out;
+        printoutBuffer = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(printoutBuffer));
+    }
+
+    @AfterEach
+    public void cleanUpStreams() {
+        printoutBuffer = null;
+        System.setOut(originalSysOut);
+    }
+
     @Test
     public void testDoShowTotalIncome() {
-        instance  = new TotalRevenueView();
-        printoutBuffer = new ByteArrayOutputStream();
-        PrintStream inMemSysOut = new PrintStream(printoutBuffer);
-        originalSysOut = System.out;
-        System.setOut(inMemSysOut);
-        
+        instance = new TotalRevenueView();
+
         Amount totalIncome = new Amount(228);
-       instance.doShowTotalIncome(totalIncome);
-      
-       String printout = printoutBuffer.toString();
-       String expectedOutputTotalIncome = "228 SEK";
-       assertTrue(printout.contains(expectedOutputTotalIncome), "Total income is not printed out.");
+        instance.doShowTotalIncome(totalIncome);
+
+        String printout = printoutBuffer.toString();
+        String expectedOutputTotalIncome = "228 SEK";
+        assertTrue(printout.contains(expectedOutputTotalIncome), "Total income is not printed out.");
     }
-    
+
 }
